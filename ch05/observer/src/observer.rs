d65b430d517a3082b7bb4932ae7b5f8ac8ce4d90
renyi <any-yi@outlook.com>
2025-04-16 17:06:37 +0800

    添加一些观察者模式的注释

--- ch05/observer/src/observer.rs
+++ ch05/observer/src/observer.rs
@@ -1,3 +1,8 @@
+// 观察者 trait
+// 该 trait 保存两个东西
+// 一个是感兴趣的主题（可以理解为一个消息队列），
+// 另一个是当感兴趣的主题消息发生时需要调用的代码（回调函数），
+//   该回调函数被传入了主题，因此可以通过这个传入参数读取主题状态
 pub trait Observer {
     type Subject;
     fn observe(&self, subject: &Self::Subject);
--- ch05/observer/src/subject.rs
+++ ch05/observer/src/subject.rs
@@ -2,7 +2,9 @@ use std::sync::{Arc, Weak};
 
 use crate::{observable::Observable, observer::Observer};
 
+// subject 有主题、起因的意思
 pub struct Subject {
+    // subject 里面保存了观察者队列
     observers: Vec<Weak<dyn Observer<Subject = Self>>>,
     state: String,
 }
@@ -20,17 +22,22 @@ impl Subject {
     }
 }
 
+// 使 subject 可被观察
 impl Observable for Subject {
     type Observer = Arc<dyn Observer<Subject = Self>>;
+    // 调用每个观察者的 observe 方法，通知被观察的 subject 状态已改变
     fn update(&self) {
         self.observers
             .iter()
             .flat_map(|o| o.upgrade())
-            .for_each(|o| o.observe(self));
+            .for_each(|o| o.observe(self)); // 注意 observe
+                                            // 方法被传入了主题，为了让观察者能够读取主题的状态
     }
+    // 观察者入队
     fn attach(&mut self, observer: Self::Observer) {
         self.observers.push(Arc::downgrade(&observer));
     }
+    // 观察者出队
     fn detach(&mut self, observer: Self::Observer) {
         self.observers
             .retain(|f| !f.ptr_eq(&Arc::downgrade(&observer)));
